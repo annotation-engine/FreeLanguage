9721a16f614501618eb3c61fee956d8cf72d803d
李佳伟 <2965502228@qq.com>
2025-11-05 19:21:24 +0800

    fix: 修复文件读取错误

--- free-core/src/nativeMain/kotlin/free/core/io/File.kt
+++ free-core/src/nativeMain/kotlin/free/core/io/File.kt
@@ -37,7 +37,7 @@ class File(
 		}
 	
 	fun readFileChars(): CharArray {
-		val file = fopen(path, "r") ?: error("Cannot open file: $path")
+		val file = fopen(path, "rb") ?: error("Cannot open file: $path")
 		try {
 			val buffers = StringBuilder()
 			memScoped {
@@ -45,7 +45,8 @@ class File(
 				while (true) {
 					val bytesRead = fread(buf, 1u, 4096u, file)
 					if (bytesRead == 0UL) break
-					buffers.append(buf.toKString())
+					val chunk = buf.readBytes(bytesRead.toInt()).decodeToString()
+					buffers.append(chunk)
 				}
 			}
 			return buffers.toString().toCharArray()
@@ -54,6 +55,28 @@ class File(
 		}
 	}
 }
+
+// fun readFileChars(path: String): CharArray {
+//    val file = fopen(path, "rb") ?: error("Cannot open file: $path")
+//
+//    try {
+//        val builder = StringBuilder()
+//        memScoped {
+//            val buf = allocArray<ByteVar>(4096)
+//            while (true) {
+//                val bytesRead = fread(buf, 1u, 4096u, file)
+//                if (bytesRead == 0UL) break
+//
+//                // 把读取的部分手动转换为字符串（确保不会读出垃圾）
+//                val chunk = buf.readBytes(bytesRead.toInt()).decodeToString()
+//                builder.append(chunk)
+//            }
+//        }
+//        return builder.toString().toCharArray()
+//    } finally {
+//        fclose(file)
+//    }
+//}
 
 fun currentWorkingDirectory(): String {
 	val cwdPtr = getcwd(null, 0u)
--- free-core/src/nativeMain/kotlin/free/core/lexer/recognizer/EOFRecognizer.kt
+++ free-core/src/nativeMain/kotlin/free/core/lexer/recognizer/EOFRecognizer.kt
@@ -7,6 +7,6 @@ data object EOFRecognizer : TokenRecognizer {
 	
 	override suspend fun tryParse(input: CharArray, start: Int, line: Int, column: Int): FreeToken? {
 		if (start < input.size) return null
-		return FreeToken(FreeTokenType.EOF, "", input.size, input.size, line, column)
+		return FreeToken(FreeTokenType.EOF, "", start, start + 1, line, column)
 	}
 }
