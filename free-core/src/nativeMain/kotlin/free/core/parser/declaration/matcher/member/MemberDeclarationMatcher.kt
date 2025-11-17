d2f8d9aad120d7a96a921c8ab5b779705c8df1d8
李佳伟 <2965502228@qq.com>
2025-11-17 14:23:10 +0800

    feat: 支持识别缺少 '}'

--- free-core/src/nativeMain/kotlin/free/core/parser/declaration/matcher/member/MemberDeclarationMatcher.kt
+++ free-core/src/nativeMain/kotlin/free/core/parser/declaration/matcher/member/MemberDeclarationMatcher.kt
@@ -2,10 +2,12 @@ package free.core.parser.declaration.matcher.member
 
 import free.core.FreeContext
 import free.core.exception.syntaxError
+import free.core.lexer.FreeTokenType
 import free.core.parser.FreeParserContext
 import free.core.parser.Modifier
 import free.core.parser.declaration.Declaration
 import free.core.parser.declaration.TypeKind
+import free.core.parser.declaration.TypeKind.*
 
 sealed interface MemberDeclarationMatcher<out D : Declaration> {
 	
@@ -45,7 +47,25 @@ fun parseMemberDeclaration(
 	modifiers: Set<Modifier>
 ): Declaration {
 	val matcher = matchers.find { it.match(ctx) }
-		?: syntaxError("未知的成员声明", ctx.current)
+		?: run {
+			if (ctx.current.type == FreeTokenType.EOF) {
+				syntaxError("${parentTypeKind.getName()}缺少 '}'", ctx.current)
+			} else {
+				syntaxError("未知的成员声明", ctx.current)
+			}
+		}
 	matcher.check(ctx, parentTypeKind, parentModifiers, modifiers)
 	return matcher.parse(ctx, parentTypeKind, modifiers)
+}
+
+private fun TypeKind.getName(): String {
+	return when (this) {
+		CLASS -> "类"
+		SINGLE -> "单例类"
+		INTERFACE -> "接口"
+		STRUCT -> "结构体"
+		ENUM -> "枚举"
+		ENUM_ENTRY -> "枚举实例"
+		ANNOTATION -> "注解"
+	}
 }
--- free-core/src/nativeMain/kotlin/free/core/parser/statement/matcher/StatementMatcher.kt
+++ free-core/src/nativeMain/kotlin/free/core/parser/statement/matcher/StatementMatcher.kt
@@ -2,6 +2,7 @@ package free.core.parser.statement.matcher
 
 import free.core.FreeContext
 import free.core.exception.syntaxError
+import free.core.lexer.FreeTokenType
 import free.core.parser.FreeParserContext
 import free.core.parser.statement.Statement
 
@@ -20,6 +21,12 @@ private val matchers = listOf(
 context(_: FreeContext)
 fun parseStatement(ctx: FreeParserContext): Statement {
 	val matcher = matchers.find { it.match(ctx) }
-		?: syntaxError("不支持的语句", ctx.current)
+		?: run {
+			if (ctx.current.type == FreeTokenType.EOF) {
+				syntaxError("函数缺少 '}'", ctx.current)
+			} else {
+				syntaxError("不支持的语句", ctx.current)
+			}
+		}
 	return matcher.parse(ctx)
 }
